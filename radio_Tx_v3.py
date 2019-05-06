5193ee8c93a41f279d723c2b5d6ae8b594b45811
Matthew Roberts <robertsmatt97@gmail.com>
2019-05-06 16:05:20 -0400

    Added message prep functions

--- radio_Tx_v3.py
+++ radio_Tx_v3.py
@@ -5,7 +5,6 @@ Matthew Roberts
 4/26/2019
 RadioRx
 This file manages the Rx for the communication system
-Stable Release
 
 """
 # Import Libraries
@@ -41,6 +40,9 @@ except RuntimeError:
 # Configure LoRa parameters
 rfm9x.tx_power = 23
 
+# CRC variables
+append_CRC = 231
+
 # Counter and index variables
 counter = 1
 start_index = 0
@@ -51,13 +53,67 @@ start_of_stream = b'\x01\x02\x03\x04\x05'
 end_of_stream = b'\x05\x04\x03\x02\x01'
 reply_packet = bytearray([90,230,17])
 
-
+# File stream variables
+target_path = os.getcwd() + "/targets/"
+target_list = sorted(os.listdir(target_path))
+os.path.abspath(target_path + target_list[0])
 # Start of Program
 print('Program Start')
 
+# Functions
+"""
+
+For Polynomial 0xe7, remainer is 0x1cf
+append:  1110 0111
+modulo:  1 1100 1111
+Max message length is: 247
+
+246 bytes left
+1 bytes of header information free
+245 bytes for pixels
+3 bytes per pixels
+81.66 pixels per message
+~ 950 packages per 320x240 image
+To send a 320x240 image in about a minute, you need at least 50 kbps.
+Should try to get between 50 to 100 kbps
+
+"""
+# Image to pixels and message (built upon javl's slowimage_sender.py functions)
+def build_pixels(image_path)
+    img_build = Image.open(image_path, mode='r')
+    width, height = img_build.size
+    pixels = []
+    rgb_img_build = img_build.convert('RGB')
+    for x_coord in range (0,width)
+        for y_coord in range (0,height)
+            red, green, blue - rgb_img_build.getpixel((x_coord,y_coord))
+            pixels.append(red)
+            pixels.append(green)
+            pixels.append(blue)
+    return pixels
+
+def build_message(pixels, start_index, end_index, part_of_message = 'middle')
+
+    msg = []
+    counter_build = start_index
+    if (part_of_message == 'start'):
+        part_int = 0
+    elif (part_of_message == 'end'):
+        part_int = 255
+    else:
+        part_int = 170
+    msg.append((part_int).to_bytes(1, byteorder='big'))
+    while ((counter_build < end_index) and (counter_build < len(pixels))):
+        msg.append(pixels[counter_build])
+        counter_build += 1
+
+    msg.append((append_CRC).to_bytes(1, byteorder='big'))
+    return msg
+
+# main
+
 while True:
-    target_path = os.getcwd() + "/targets/"
-    target_list = sorted(os.listdir(target_path))
+
     for f_stream in target_list:
         if (f_stream.endswith(".png") == False):
             print(f_stream)
